04696b801da778568c9c15affb7c24d7ca22ad52
nickwells <wellsnick11@yahoo.com>
2022-01-25 07:35:24 -0800

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+
+  const newTaskForm = document.getElementById ("create-task-form")
+  const newTaskDescription = document.getElementById ("new-task-description")
+  const newTaskPriority = document.getElementById ("new-task-priority")
+
+  const taskUl = document.getElementById("tasks")
+
+  newTaskForm.addEventListener("submit", createTask)
+})
+
+const createTask = e => {
+  e.preventDefault()
+  
+  const newTaskDescription = document.getElementById("new-task-description")
+  const newTask = document.createElement("li")
+  newTask.innerText = newTaskDescription.value
+
+  appendNewTask(newTask)
+  e.target.reset() 
+}
+
+  const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task)
+  }
